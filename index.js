ec1d7e73e48bd15b6ec47f5673b1ca23be547f29
parweb <parweb@gmail.com>
2017-04-27 22:08:12 +0200

    edit example

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+var app = require('express')();
+var srv = require('http').createServer(app);
+var bodyParser = require('body-parser');
+
+app.get('/', function (req, res, next) {
+  res.send('Hello world');
+});
+
+srv.listen(3000, function () {
+  console.log('Listening on 3000');
+});
